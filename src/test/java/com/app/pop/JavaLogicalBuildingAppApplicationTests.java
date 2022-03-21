ce560976e83e9323cac58478b7770bda59384a7f
ZeeshanAli <ZeeshanAli@ZeeshanAli-PC>
2022-03-21 20:01:15 +0530

    first commit

---
+++ src/test/java/com/app/pop/JavaLogicalBuildingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLogicalBuildingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
