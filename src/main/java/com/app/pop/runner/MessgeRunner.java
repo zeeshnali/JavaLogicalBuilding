e8666fbcf9941d4c083f953a07095eb8c4b04ebb
ZeeshanAli <ZeeshanAli@ZeeshanAli-PC>
2022-03-21 19:09:25 +0530

    zali17-first-cmmit

---
+++ src/main/java/com/app/pop/runner/MessgeRunner.java
@@ -0,0 +1,13 @@
+package com.app.pop.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessgeRunner implements CommandLineRunner {
+
+	public void run(String... args) throws Exception {
+		System.out.println("welcome to boot app");
+
+	}
+
+}
