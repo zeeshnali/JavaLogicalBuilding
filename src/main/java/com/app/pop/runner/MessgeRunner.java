d03772a77a592f3b6cdc8c276eb7d3ef3296aebf
ZeeshanAli <ZeeshanAli@ZeeshanAli-PC>
2022-03-21 20:37:50 +0530

    second push

--- src/main/java/com/app/pop/runner/MessgeRunner.java
+++ src/main/java/com/app/pop/runner/MessgeRunner.java
@@ -7,6 +7,8 @@ public class MessgeRunner implements CommandLineRunner {
 
 	public void run(String... args) throws Exception {
 		System.out.println("welcome to boot app");
+		System.out.println("welcome to boot app");
+		System.out.println("welcome to boot app");
 
 	}
 
